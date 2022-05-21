255054e5e2275b92e4b1977be65f40493b2f1fe2
Danil Sokolov <danilzyx@gmail.com>
2022-05-21 17:58:34 +0300

    adding init command

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
-import shutil
-import pathlib
 
 setup(
     name='muni',
-    version='0.1.0',
+    version='0.1.3',
     packages=['muni'],
     install_requires=[
         'click >= 8.1.3',
+        'aiogram >= 2.2.0',
+        'aioredis >= 2.0.1',
+        'environs >= 9.5.0',
+        'peewee >= 3.14.10'
     ],
+    scripts=[
+        'bin/ni',
+        'bin/muni'
+    ],
+    include_package_data=True
 )
-
-shutil.copy(pathlib.Path().parent.joinpath('muni/cli/ni.py'), '/usr/local/bin/ni')
